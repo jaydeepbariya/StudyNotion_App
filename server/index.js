ae196de125b128f433071017d4c4e9889157d4f3
unknown <jaydeepbariya59@gmail.com>
2023-08-28 16:46:20 +0530

    cors chage

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://backend-studynotion-mn3b.onrender.com/api/v1",
 		credentials:true,
 	})
 )
