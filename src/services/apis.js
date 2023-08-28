cf040dd3fcf56e8f315ad7fa25c9444fa4bd9361
unknown <jaydeepbariya59@gmail.com>
2023-08-28 16:36:10 +0530

    url deployment

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://backend-studynotion-mn3b.onrender.com/api/v1";
+const BASE_URL = process.env.BASE_URL
 
 export const categories = {
   CATEGORIES_API: BASE_URL + "/course/showAllCategories",
